bd7a3ffd768010a76e84817d93507a3296bba703
guga1109 <gustavocbevilacqua@gmail.com>
2022-07-03 17:40:09 -0300

    Changed browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Main from './layout/Main';
 import './style/main.css';
 import Home from './pages/Home';
@@ -9,15 +9,15 @@ import Contact from './pages/Contact';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes basename="gustavosportfolio">
+        <HashRouter>
+            <Routes>
                 <Route path="/" element={<Main/>}>
                     <Route index element={<Home/>}/>
                     <Route path="books" element={<Books/>}/>
                     <Route path="contact" element={<Contact/>}/>
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
