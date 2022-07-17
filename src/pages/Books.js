dbfa21c0ce3c3e9ed41dd07c6e79d3763ef0e620
guga1109 <gustavocbevilacqua@gmail.com>
2022-07-17 09:10:13 -0300

    book page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CleanCode from "../assets/images/cleancode.jpeg";
 
 const Books = () => {
+    const [minimized, setMinimized] = useState(false);
+
     return (
       <div className="row full-height">
-        <div className="flex justify-evenly column-6 text-white">
+        <div
+          className={
+            "width-transition flex justify-evenly text-white padding-no-left " +
+            (minimized ? "column-6" : "column-12")
+          }
+        >
           <div className="book-box">
             <div className="book-image">
               <img src={CleanCode} />
@@ -14,70 +21,29 @@ const Books = () => {
                 <h1>Book one</h1>
               </div>
               <div className="book-summary">
-                Even bad code can function. But if code isn't clean, it can
-                bring a development organization to its knees. Every year,
-                countless hours and significant resources are lost because of
-                poorly written code. But it doesn't have to be that way. Noted
-                software expert Robert C. Martin presents a revolutionary
-                paradigm with Clean Code: A Handbook of Agile Software
-                Craftsmanship. Martin has teamed up with his colleagues from
-                Object Mentor to distill their best agile practice of cleaning
-                code "on the fly" into a book that will instill within you the
-                values of a software craftsman and make you a better
-                programmer--but only if you work at it.
+                Okay this is summary for the first book!
               </div>
             </div>
             <div className="see-more">
-              <button>
+              <button onClick={() => setMinimized(true)}>
                 <span className="material-icons">arrow_right</span>
               </button>
             </div>
           </div>
-          <div className="book-box">
-            <div className="book-image">
-              <img src={CleanCode} />
-            </div>
-            <div className="content">
-              <div className="book-title">
-                <h1>Book one</h1>
-              </div>
-              <div className="book-summary">
-                Even bad code can function. But if code isn't clean, it can
-                bring a development organization to its knees. Every year,
-                countless hours and significant resources are lost because of
-                poorly written code. But it doesn't have to be that way. Noted
-                software expert Robert C. Martin presents a revolutionary
-                paradigm with Clean Code: A Handbook of Agile Software
-                Craftsmanship. Martin has teamed up with his colleagues from
-                Object Mentor to distill their best agile practice of cleaning
-                code "on the fly" into a book that will instill within you the
-                values of a software craftsman and make you a better
-                programmer--but only if you work at it.
-              </div>
-            </div>
+        </div>
+        <div
+          className={
+            "window flex width-transition text-white " +
+            (minimized ? "column-6" : "column-0")
+          }
+        >
+          <div className="top-bar">
+            <button onClick={() => setMinimized(false)}>
+              <span className="material-icons">close</span>
+            </button>
           </div>
-          <div className="book-box">
-            <div className="book-image">
-              <img src={CleanCode} />
-            </div>
-            <div className="content">
-              <div className="book-title">
-                <h1>Book one</h1>
-              </div>
-              <div className="book-summary">
-                Even bad code can function. But if code isn't clean, it can
-                bring a development organization to its knees. Every year,
-                countless hours and significant resources are lost because of
-                poorly written code. But it doesn't have to be that way. Noted
-                software expert Robert C. Martin presents a revolutionary
-                paradigm with Clean Code: A Handbook of Agile Software
-                Craftsmanship. Martin has teamed up with his colleagues from
-                Object Mentor to distill their best agile practice of cleaning
-                code "on the fly" into a book that will instill within you the
-                values of a software craftsman and make you a better
-                programmer--but only if you work at it.
-              </div>
-            </div>
+          <div className="window-body">
+            test
           </div>
         </div>
       </div>
